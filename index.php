a4bc308bed6a9bac82babf857bb592724aca3816
Vickie Chen <wchen@uams.edu>
2016-01-14 15:52:16 -0500

    init setup

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php include("includes/header.inc");?>  
+<?php include("slides.html");?>     
+
+<!-- Wrap the rest of the page in another container to center all the content. -->	
+<!-- Add Angular Controller -->
+<div class="tab-content">		
+	<?php include("home.html");?>  
+	<?php include("panels.html");?>  		
+	<?php include("portfolio.html");?>  		
+</div>
+<hr>
+
+<?php include("includes/footer.inc");?>
---
+++ js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function (){	
+	
+	// Add smooth scrolling to all links in navbar link
+   $("#navbar a, .details").on('click', function(event) { 
+		if(this.hash.indexOf("#")==0){
+		  // Prevent default anchor click behavior
+		  event.preventDefault();
+
+		  // Store hash
+		  var hash = this.hash;
+		  var pos = $(this).offset().top;
+	
+		  //  var project = (hash=="portfolio"?$(this).attr("for"):""); 
+		  //  pos = (project!=""?$("#"+project).offset().top:pos);
+		  
+		  // Using jQuery's animate() method to add smooth page scroll
+		  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+		  $('body').animate({
+			scrollTop: pos
+		  }, 100, function(){
+
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+			});
+		}
+	});	
+	
+	$('#navbar').tab();	
+	$('[data-toggle="tooltip"]').tooltip(); 
+	
+	//document.onmousedown=disableclick;
+	//status="Right Click Disabled";
+
+ });
+
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('homeController', function($scope,$http){
+	$http.get('data/home.json').success(function(data) {
+		$scope.home=data;	
+		$scope.$watch('home', function(newValue,oldValue){
+			//console.log("new value is "+newValue);
+	   });
+	});
+});
+
+function disableclick(event){
+  if(event.button==2) return false;    
+}
+
